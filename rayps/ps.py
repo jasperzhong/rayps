e2b22800d8fe25e07975fa420e1551ae8d93f5c2
vycezhong <izhongyuchen@gmail.com>
2021-05-04 23:45:43 +0800

    fix contention

--- rayps/ps.py
+++ rayps/ps.py
@@ -12,29 +12,36 @@ class BytePS:
         self.n_recv = {}
         self.ready_for_pull = {}
         self.buffer = {}
+        self.store = {}
         self.locks = {}
 
         for key, param in enumerate(model.parameters()):
             if param.requires_grad:
                 self.n_recv[key] = 0
                 self.ready_for_pull[key] = False
-                self.buffer[key] = np.zeros(param.size())
+                self.store[key] = np.zeros(param.size())
                 self.locks[key] = Lock()
 
     def push(self, key, grad):
         self.locks[key].acquire()
         if self.n_recv[key] == 0:
-            self.buffer[key].fill(0)
-        self.buffer[key] += grad
+            # copy is needed here.
+            # https://github.com/ray-project/ray/issues/369
+            self.buffer[key] = grad.copy()
+        else:
+            self.buffer[key] += grad
         self.n_recv[key] += 1
         if self.n_recv[key] == self.n_workers:
             self.buffer[key] /= self.n_workers
+            self.store[key][:] = self.buffer[key]
             self.ready_for_pull[key] = True
         self.locks[key].release()
 
     def pull(self, key):
         while not self.ready_for_pull[key]:
             time.sleep(0.001)
+        self.locks[key].acquire()
         self.ready_for_pull[key] = False
         self.n_recv[key] = 0
-        return self.buffer[key]
+        self.locks[key].release()
+        return self.store[key]
